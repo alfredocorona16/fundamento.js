47ae0ec346003a46a344c91f777a23a42d053c9a
alfredocorona16 <alfredcorona16@gmail.com>
2024-09-12 13:31:25 -0600

    01 Tipos de Dato Javascript

---
+++ app.js
@@ -0,0 +1,2 @@
+console.log("holaaaaaa");
+alert("hello word");
---
+++ consepts/datatypes.js
@@ -0,0 +1,19 @@
+// Tipos de datoen Javascript
+// numéros (enteros, decimale)
+console.log(23);
+console.log(typeof 23);
+console.log(3.1416);
+console.log(typeof 3.1416);
+
+// booleans (tru, false)
+
+console.log(true);
+console.log( typeof true);
+console.log(false);
+console.log(typeof true);
+
+console.log(undefined);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof [1, 2, 3]);
+console.log(typeof {nam: 'John'});
